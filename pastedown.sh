f4c54b7edbc26c769d12014cd2f9e67ed1644ecb
Xcaliburr101 <yordidominator@gmail.com>
2024-05-09 23:40:45 +0200

    added place, copies the file to the right location

--- pastedown.sh
+++ pastedown.sh
@@ -1,18 +1,37 @@
 #!/bin/bash
 
-set -uo pipefail
-#set error handling
+function download {
+    set -uo pipefail
+    mkdir -p "$HOME/Documents/dotties" &> /dev/null
 
-declare -A urls=(
-    [aliases.fish]="https://pastebin.com/0kPtr0QV"
-    [bashrc]="https://pastebin.com/WMmk7Cc0"
-    [config.fish]="https://pastebin.com/BZ8j6NBC"
-    [env.fish]="https://pastebin.com/8yReRyaa"
-)
+    declare -A urls=(
+         [config.fish]="https://pastebin.com/BZ8j6NBC"
+         [aliases.fish]="https://pastebin.com/0kPtr0QV"
+         [env.fish]="https://pastebin.com/8yReRyaa"
+         [zoxide.fish]="https://pastebin.com/6wW6YEvz"
+    )
 
-for url in "${!urls[@]}"; do
-    echo "Downloading $url..."
-    mkdir -p dotties &> /dev/null
-    pastebin get "${urls[$url]}" > "dotties/.$url"
-    echo "saved file to .$url"
-done
+    for url in "${!urls[@]}"; do
+        local file_path="$HOME/Documents/dotties/$url"
+        echo "Downloading $url..."
+        if pastebin get "${urls[$url]}" > "$file_path"; then
+            echo "Saved file to $file_path"
+        else
+            echo "Error: Failed to download $url" >&2
+        fi
+    done
+}
+
+function place {
+fishloc="$HOME/.config/fish/"
+cd $HOME/Documents/dotties/
+
+cp -v config.fish $fishloc
+cp -v aliases.fish $fishloc/conf.d/
+cp -v zoxide.fish $fishloc/conf.d/
+cp -v env.fish $fishloc/conf.d/
+
+}
+
+#fire
+download && place
